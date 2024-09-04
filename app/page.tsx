170ff182868d5381ba05ab75a6d52e8eaa9a824c
Ronaldo Guedes <ronaldoguedess@gmail.com>
2024-09-04 14:47:05 -0300

    settings

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,22 @@
-'use client'
+import TelegramAuth from '@/components/TelegramAuth';
+import { getSession } from '@/utils/session';
+
+
+export default async function Home() {
+  const session = await getSession()
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold mb-8">Jwt Authentication for Telegram Mini Apps</h1>
+      <pre>{JSON.stringify(session, null, 2)}</pre>
+      <TelegramAuth />
+    </main>
+  )
+}
+
+
+
+/*'use client'
 
 import WebApp from '@twa-dev/sdk'
 import { useEffect, useState } from 'react'
@@ -41,4 +59,4 @@ export default function Home() {
       )}
     </main>
   )
-}
+}*/
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    output: 'export',
-  }
+const nextConfig = {};
 
 export default nextConfig;
